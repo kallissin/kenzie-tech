60fa5563d0c8d4b6e555313dde7736134f8adfc5
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-07-22 21:36:12 -0300

    initial commit

---
+++ src/styles/global.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-size: 1rem;
+        font-family: 'Karla', sans-serif; 
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: 'Arvo', serif;
+    }
+
+    button {
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    :root {
+        --black: #201F1F;
+        --red: #FA1616;
+        --gray: #363535;
+        --gray-Light: #DBDBDB;
+        --blue: #0B1256;
+        --background-gray: #ACB9BE;
+        --background-blue: #0B1839;
+    }
+`;
+
+export default GlobalStyle;

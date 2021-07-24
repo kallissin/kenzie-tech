b0dd13b8391af73ca5562436a5973e2c898d922e
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-07-24 14:05:38 -0300

    menu dashboard concluseded

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,38 @@
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, ContainerInput, Menu } from "./styles";
+import { useForm } from "react-hook-form";
+import { SiReactos } from "react-icons/si";
+import { GiBattery100 } from "react-icons/gi";
 export const Dashboard = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <header>
+      <Menu>
+        <Container>
+          <div>
+            <h1>KenzieTech</h1>
+          </div>
+          <ContainerInput>
+            <Input
+              placeholder="Ex: React"
+              register={register}
+              name="tech"
+              label="Tecnologia"
+              icon={SiReactos}
+            />
+            <Input
+              placeholder="Ex: básico"
+              register={register}
+              name="tech"
+              label="Nível de experiencia"
+              icon={GiBattery100}
+            />
+            <Button>Enviar</Button>
+          </ContainerInput>
+        </Container>
+      </Menu>
+    </header>
   );
 };
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Menu = styled.div`
+  position: fixed;
+  width: 100%;
+  background-color: var(--gray);
+`;
+
+export const Container = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+
+  @media all and (min-width: 1100px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  div {
+    h1 {
+      font-size: 2rem;
+      color: var(--blue);
+
+      @media all and (min-width: 1100px) {
+        font-size: 3rem;
+      }
+    }
+  }
+`;
+
+export const ContainerInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media all and (min-width: 1100px) {
+    flex-direction: row;
+  }
+
+  > div {
+    width: 200px;
+    padding-top: 0;
+    @media all and (min-width: 1100px) {
+      margin-right: 1.2rem;
+    }
+  }
+
+  button {
+    margin-top: 1rem;
+    width: 120px;
+    height: 30px;
+    font-size: 1rem;
+  }
+`;

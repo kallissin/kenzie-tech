da269774d9a98dbdd1adaf7d2fad8b8464b28767
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-10-24 12:35:48 -0300

    feat: created component Profile and basic styled

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -1,50 +1,85 @@
 import { useState } from "react";
-import { Container } from "./styles";
+import { Display, Container, Header, Content } from "./styles";
+import ImageProfile from "../../assets/kelvin.jpg";
+import { Button } from "../Button";
+import { GoGear } from "react-icons/go";
 
 const ProfileInfo = () => {
-  const [edit, setEdit] = useState(true);
+  const [edit, setEdit] = useState(false);
 
   return (
-    <Container>
-      <h2>Componente ProfileInfo</h2>
-      <figure>
-        <img src="#" alt="Profile" />
-        <figcaption>Detalhe da imagem</figcaption>
-      </figure>
-      {edit ? (
-        <>
-          <p>
-            <span>Name:</span>Kelvin Alisson Cantarino
-          </p>
-          <p>
-            <span>Email:</span>Kelvin@email.com
-          </p>
-          <p>
-            <span>Contact:</span>linkdin/in/kallissin
-          </p>
-          <p>
-            <span>Bio:</span>Estudando Análise e Desenvolvimento de Sistemas
-          </p>
-          <button>Enviar</button>
-        </>
-      ) : (
-        <form>
-          <div>
-            <input></input>
-          </div>
-          <div>
-            <input></input>
-          </div>
-          <div>
-            <input></input>
-          </div>
-          <div>
-            <input></input>
-          </div>
-          <button>Salvar</button>
-        </form>
-      )}
-    </Container>
+    <Display>
+      <Container>
+        <div className="content-box">
+          <Header>
+            <figure>
+              <img src={ImageProfile} alt="Profile" />
+              <figcaption>Detalhe da imagem</figcaption>
+            </figure>
+            <div>
+              <span className="header__icon">
+                <GoGear />
+              </span>
+            </div>
+          </Header>
+          {edit ? (
+            <Content>
+              <div>
+                <p>
+                  <span>Name: </span>Kelvin Alisson Cantarino
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span>Email: </span>Kelvin@email.com
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span>Contact: </span>linkdin/in/kallissin
+                </p>
+              </div>
+              <div>
+                <p>
+                  <span>Bio: </span>Estudando Análise e Desenvolvimento de
+                  Sistemas
+                </p>
+              </div>
+              <div className="container-button">
+                <Button>Editar</Button>
+              </div>
+            </Content>
+          ) : (
+            <Content>
+              <form>
+                <div>
+                  <input
+                    className="form-input"
+                    value="Kelvin Alisson Cantarino"
+                  />
+                </div>
+                <div>
+                  <input className="form-input" value="Kelvin@email.com" />
+                </div>
+                <div>
+                  <input className="form-input" value="linkdin/in/kallissin" />
+                </div>
+                <div>
+                  <textarea
+                    className="form-input textarea"
+                    value="Estudando Análise e Desenvolvimento de
+                Sistemas"
+                  />
+                </div>
+                <div>
+                  <Button>Salvar</Button>
+                </div>
+              </form>
+            </Content>
+          )}
+        </div>
+      </Container>
+    </Display>
   );
 };
 
